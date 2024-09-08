c0797f5325134edc5e14fe5037d84c4c906242fb
Meet <meet2003j@gmail.com>
2024-09-08 08:45:18 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+let name="meet"
+let count =1
+console.log(`Hello my name is ${name} and count is ${count}`)
+
+
+let getName=new String("MeetJ")
+
+console.log(getName)
+
+console.log(getName[0])
+
+console.log(getName.charAt(2))
+
+console.log(getName.indexOf('e'))
+
+
+let subString=getName.substring(0,4)
+
+console.log(subString)
+
+console.log("After slicing")
+
+let sliceString=getName.slice(0,4)
+
+console.log(sliceString)
+
+sliceString="ram"
+
+console.log(getName)
+
+console.log(sliceString)
