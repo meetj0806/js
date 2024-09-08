cfffbc943c096981dd19e7d2ed1b8800f18c03ee
Meet <meet2003j@gmail.com>
2024-09-08 07:17:25 +0000

    operations added

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,14 +1,27 @@
-let score="Meet"
+// let score="Meet"
 // console.log(typeof score)
-let value=Number(score)
+// let value=Number(score)
 // console.log(typeof(value))
 // console.log(value)
+
 
 // "33"->33
 // "Meet"->NaN
 // "true"->1
-let isLoggedIn = "33"
-console.log(isLoggedIn," -> ",typeof isLoggedIn)
-console.log("After Conversion")
-isLoggedIn=Boolean(isLoggedIn)
-console.log(isLoggedIn," -> ",typeof isLoggedIn)
+
+
+// let isLoggedIn = "33"
+// console.log(isLoggedIn," -> ",typeof isLoggedIn)
+// console.log("After Conversion")
+// isLoggedIn=Boolean(isLoggedIn)
+// console.log(isLoggedIn," -> ",typeof isLoggedIn)
+
+// **************************x******************************
+
+let value=3
+let negvalue=-value
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
