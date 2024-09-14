960f1db0aa8be5b5ce993bcaeb2d49a84a3aca56
Meet <meet2003j@gmail.com>
2024-09-14 13:09:27 +0000

    Math

---
+++ 01_basics/06_Math.js
@@ -0,0 +1,11 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4.5));
+
+
+// console.log((Math.random()*10)%6);
+
+
+const min=10;
+const max=20;
+console.log(Math.floor((Math.random()*(max-min+1)+min)));
